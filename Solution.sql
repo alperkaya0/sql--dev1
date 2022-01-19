b31adf42c43f171c4f4cbc16852f865dc3a4b18d
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-01-19 04:07:49 +0300

    Update Solution.sql

--- Solution.sql
+++ Solution.sql
@@ -4,6 +4,18 @@
 --SELECT last_name from customer WHERE first_name = 'Mary';
 --SELECT * FROM film WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
 
+ENGLISH:
+--Hello,
+
+--Please write SQL code of the scenarios by using the example data table dvdrental.
+
+--1. Query the title and description columns that reside at film table.
+--2. Query all of the columns of the film table that have length bigger than 60 AND smaller than 75.
+--3. Query all of the columns of the film table that have rental_rate equals 0.99 AND replacement_cost equals 12.99 OR 28.99.
+--4. What is the last_name value of the customer that has value of 'Mary' at it's first_name column that resides at customer table.
+--5. Query all of the columns of the film table that have length NOT bigger than 50 at the same time rental_rate doesn't equal to 2.99 OR 4.99.
+
+TURKISH:
 --Merhabalar,
 
 --Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
@@ -13,3 +25,4 @@
 --3.film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
 --4.customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 --5.film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+
